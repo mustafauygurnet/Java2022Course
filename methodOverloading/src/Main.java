c15da7d7c85d5950f0a7f4d3065a9a9999f8222d
Mustafa UYGUR <mustafauygur@outlook.com>
2022-09-22 15:08:30 +0300

    method overloading has been added

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MathClass mathClass = new MathClass();
+        System.out.println(mathClass.sum(1,4));
+        System.out.println(mathClass.sum(1,4,5));
+    }
+}
---
+++ methodOverloading/src/MathClass.java
@@ -0,0 +1,4 @@
+public class MathClass {
+    public int sum(int number1, int number2){return number1+number2;}
+    public int sum(int number1, int number2,int number3){return number1+number2+number3;}
+}
